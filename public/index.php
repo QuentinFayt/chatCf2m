0d991a03beed8a0e9a45da84253edd4048fc2df7
QuentinFayt <quentin.fayt.p@gmail.com>
2021-11-19 11:25:12 +0100

    init style-script + link

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 require_once "../config/config.php";
 
-include_once "../view/public/head.php";
+include_once "../view/head.php";
 include_once "../view/public/" . (isset($_GET["p"]) && in_array($_GET["p"], WHITE_LIST) ? $_GET["p"] : "Homepage") . ".php";
-include_once "../view/public/foot.php";
+include_once "../view/foot.php";
