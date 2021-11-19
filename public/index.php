6388da26efa78908e5a3d8b2a6020daebec6b452
QuentinFayt <quentin.fayt.p@gmail.com>
2021-11-19 11:12:20 +0100

    frontal controller ini

--- public/index.php
+++ public/index.php
@@ -1,1 +1,7 @@
 <?php
+
+require_once "../config/config.php";
+
+include_once "../view/public/head.php";
+include_once "../view/public/" . (isset($_GET["p"]) && in_array($_GET["p"], WHITE_LIST) ? $_GET["p"] : "Homepage") . ".php";
+include_once "../view/public/foot.php";
