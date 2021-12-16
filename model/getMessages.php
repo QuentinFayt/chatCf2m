1f02e3c8c767c96d8cf0a96d6bbc4a1ea0a1edd8
QuentinFayt <quentin.fayt.p@gmail.com>
2021-12-16 08:02:56 +0100

    load new messages from separated script

--- model/getMessages.php
+++ model/getMessages.php
@@ -1,9 +1,10 @@
 <?php
 
-function getMessages(mysqli $db, $limit, $offset = 0)
+function getMessages(mysqli $db, $limit, $offset = 0): ?array
 {
 
     $sql = "SELECT m.messages_id,m.message, m.date,u.displayedName,u.users_id FROM `chatcf2m_messages` m INNER JOIN chatcf2m_users u ON m.users_id = u.users_id ORDER BY m.date DESC LIMIT $limit OFFSET $offset;";
 
-    return mysqli_fetch_all(mysqli_query($db, $sql), MYSQLI_ASSOC);
+    $request = mysqli_query($db, $sql) or die(mysqli_error($db));
+    return mysqli_fetch_all($request, MYSQLI_ASSOC);
 }
--- public/assets/api/loadNewMessages.php
+++ public/assets/api/loadNewMessages.php
@@ -2,9 +2,12 @@
 session_start();
 require_once "../../../config/config.php";
 require_once "../../../model/dbConnexion.php";
-require_once "../../../model/getMessages.php";
+require_once "../../../model/getNewMessages.php";
 
 
-/* if (isset($_SESSION["sessionID"]) && $_SESSION["sessionID"] == session_id()) {
-    echo json_encode(getMessages($DB));
-} */
+if (isset($_SESSION["sessionID"]) && $_SESSION["sessionID"] == session_id()) {
+    if (isset(array_keys($_GET)[0])) {
+        $lastId = (int) array_keys($_GET)[0];
+        echo json_encode(getNewMessages($DB, $lastId));
+    }
+}
